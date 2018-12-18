13190fc6eefb17db3f8cf265286f17649e981ba9
Attila Marosi <marosi.attis@gmail.com>
2018-12-18 10:23:35 +0100

    Initial Commit

---
+++ UINavigationControllerProgrammatically/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UINavigationControllerProgrammatically
+//
+//  Created by Attila Marosi on 2018. 12. 18..
+//  Copyright © 2018. Attila Marosi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
